e0d0c7e2bba11e443f2aa45d4bf862cfcc313a5f
unknown <syyadkhan@yahoo.com>
2018-06-21 16:39:48 -0400

    gravitar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/User');
+
 
 router.get('/test', (req, res) => res.json({msg: "Users Works"}));
+
+
+router.post('/register', (res, res) => {
+    User.findOne({ email: req.body.email })
+    .then(user => {
+        if(user){
+            return res.statusCode(400).json({ email: 'Email already exists'});
+        }else{
+            const newUser = new User({
+               name: req.body.name,
+                email: req.body.email,
+                avatar,
+                password: req.body.password
+            });
+        }
+    })
+});
+
+
+
+
 
 module.exports = router;
 
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const gravatar = require('gravatar');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //Connect to MongoDB
 mongoose.connect("mongodb://localhost/cfdn");
