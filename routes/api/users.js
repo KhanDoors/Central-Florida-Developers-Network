9c65784f30f33ceb7cedee8d26d564b0ffdbad44
unknown <syyadkhan@yahoo.com>
2018-06-21 18:03:48 -0400

    tokenimplemented

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 const User = require('../../models/User');
 
@@ -55,7 +57,15 @@ router.post('/login', (req, res) => {
 //check password
         bcrypt.compare(password, user.password).then(isMatch => {
             if(isMatch){
-                res.json({msg: 'Success'});
+const payload = { id: user.id, name: user.name, avatar: user.avatar}
+
+                jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, 
+                    (err, token) => {
+                        res.json({
+                            success: true,
+                            token: 'Bearer ' + token
+                        });
+                    });
             }else{
                 return res.status(400).json({password: 'Password Flawed'});
             }
