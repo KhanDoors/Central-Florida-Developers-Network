6fcd6901f672467f4c1b3ae4e58f2c120f3f69bd
unknown <syyadkhan@yahoo.com>
2018-06-21 17:46:02 -0400

    login

--- routes/api/users.js
+++ routes/api/users.js
@@ -42,7 +42,27 @@ router.post('/register', (req, res) => {
     });
 });
 
+//user login 
 
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email}).then(user => {
+        if(!user){
+            return res.status(404).json({email: 'User not here, come back later'});
+        }
+//check password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if(isMatch){
+                res.json({msg: 'Success'});
+            }else{
+                return res.status(400).json({password: 'Password Flawed'});
+            }
+        });
+
+    });
+});
 
 
 
