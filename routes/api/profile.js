963330582cf6905ad8bdeb8a89304c02dba57e14
unknown <syyadkhan@yahoo.com>
2018-06-24 06:42:06 -0400

    june240642

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -33,6 +33,38 @@ router.get('/', passport.authenticate('jwt', {
 });
 
 
+router.get('/handle/:handle', (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+        if(!profile){
+            errors.noprofile = 'There is no Profile for this user';
+            res.status(404).json(errors);
+        }
+        res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+router.get('/user/:user_id', (req, res) => {
+    const errors = {};
+    
+    Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+        if(!profile){
+            errors.noprofile = 'There is no Profile for this user';
+            res.status(404).json(errors);
+        }
+        res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+
+
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
 
 const { errors, isValid } = validateProfileInput(req.body);
