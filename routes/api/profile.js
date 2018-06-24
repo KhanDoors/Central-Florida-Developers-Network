f29b2d132ba3623fa05d266fa13012c788dc6d22
unknown <syyadkhan@yahoo.com>
2018-06-24 06:15:15 -0400

    validations

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
 
+
+const validateProfileInput = require('../../validation/profile');
+
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
@@ -18,6 +21,7 @@ router.get('/', passport.authenticate('jwt', {
     Profile.findOne({
             user: req.user.id
         })
+        .populate('user', ['name', 'avatar'])
         .then(profile => {
             if (!profile) {
                 errors.noprofile = 'There is no profile for this user';
@@ -30,6 +34,13 @@ router.get('/', passport.authenticate('jwt', {
 
 
 router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+const { errors, isValid } = validateProfileInput(req.body);
+
+if(!isValid){
+    return res.status(400).json(errors);
+}
+
    const profileFields = {};
    profileFields.user = req.user.id;
    if(req.body.handle) profileFields.handle = req.body.handle;
@@ -45,11 +56,11 @@ if(typeof req.body.skills != 'undefined') {
 }
 
 profileFields.social = {};
-   if(req.body.youtube) profileFields.youtube = req.body.youtube;
-   if(req.body.twitter) profileFields.twitter = req.body.twitter;
-   if(req.body.facebook) profileFields.facebook = req.body.facebook;
-   if(req.body.instagram) profileFields.instagram = req.body.instagram;
-   if(req.body.linkedin) profileFields.linkedin = req.body.linkedin;
+   if(req.body.youtube) profileFields.social.youtube = req.body.youtube;
+   if(req.body.twitter) profileFields.social.twitter = req.body.twitter;
+   if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
+   if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
+   if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
 
    Profile.findOne({ user: req.user.id})
    .then(profile => {
