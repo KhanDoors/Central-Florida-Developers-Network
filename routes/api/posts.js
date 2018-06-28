47ccf2789fcc151c22e237104201c4203f0de50a
unknown <syyadkhan@yahoo.com>
2018-06-28 19:23:28 -0400

    added comment route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -97,4 +97,39 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
 
 
 
+
+router.post(
+    '/comment/:id',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+      const { errors, isValid } = validatePostInput(req.body);
+  
+      // Check Validation
+      if (!isValid) {
+        // If any errors, send 400 with errors object
+        return res.status(400).json(errors);
+      }
+  
+      Post.findById(req.params.id)
+        .then(post => {
+          const newComment = {
+            text: req.body.text,
+            name: req.body.name,
+            avatar: req.body.avatar,
+            user: req.user.id
+          };
+  
+          // Add to comments array
+          post.comments.unshift(newComment);
+  
+          // Save
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+    }
+  );
+
+
+
+
 module.exports = router;
