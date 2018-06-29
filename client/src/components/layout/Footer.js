cc8d2901372fd653adfa63769215f7ac293b3ddd
unknown <syyadkhan@yahoo.com>
2018-06-29 12:07:03 -0400

    landingnavbarfooter

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default () => {
+  return (
+   <footer className="bg-dark text-warning mt-5 p-4 text-center">
+   Copyright &copy; {new Date().getFullYear()} Central Florida Developers Network </footer>
+  )
+}
