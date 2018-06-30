9beb97bcbaf972871b0735bbda30db4afcbd734c
unknown <syyadkhan@yahoo.com>
2018-06-29 20:22:12 -0400

    UIchange

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -9,9 +9,9 @@ class Landing extends Component {
           <div className="container">
             <div className="row">
               <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Network
+                <h1 className="display-3 mb-4">Central Florida Developers Network
                 </h1>
-                <p className="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
+                <p className="lead"> Publicize your Profile/Portfolio and Collaborate with other Developers</p>
                 <hr />
                 <Link to="/register" className="btn btn-lg btn-info mr-2">Sign Up</Link>
                 <Link to="/login" className="btn btn-lg btn-light">Login</Link>
