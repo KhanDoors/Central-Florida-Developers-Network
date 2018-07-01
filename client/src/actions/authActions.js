f5051c2984807e41fd3d1abecc9b6b3025317844
unknown <syyadkhan@yahoo.com>
2018-07-01 10:14:39 -0400

    workingonlogin

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
-import { GET_ERRORS } from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode';
+
+
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
 
 
 export const registerUser = (userData, history) => dispatch => {
@@ -13,3 +18,27 @@ export const registerUser = (userData, history) => dispatch => {
     })
     );
 };
+
+export const loginUser = userData => dispatch => {
+    axios.post('/api/users/login', userData)
+    .then(res => {
+const { token } = res.data;
+localStorage.setItem('jwtToken', token);
+setAuthToken(token);
+const decoded = jwt_decode(token);
+dispatch(setCurrentUser(decoded));
+
+    })
+    .catch(err => 
+        dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    }));
+};
+
+export const setCurrentUser = (decoded) => {
+    return {
+        type: SET_CURRENT_USER,
+        payload: decoded
+    }
+}
