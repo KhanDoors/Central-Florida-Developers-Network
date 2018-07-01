0e846726b8f04c7407d0a325426cd09ee1eb0503
unknown <syyadkhan@yahoo.com>
2018-06-30 21:36:47 -0400

    reduxImplemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -9,9 +12,12 @@ import Login from './components/auth/Login';
 
 import './App.css';
 
+
+
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <Router>
       <div className="App">
         <Navbar />
@@ -23,6 +29,7 @@ class App extends Component {
         <Footer />
       </div>
       </Router>
+      </Provider>
     );
   }
 }
