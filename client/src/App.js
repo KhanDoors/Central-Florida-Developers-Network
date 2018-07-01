023426ed1ce5c9bbf4ec72af538596829bca3394
unknown <syyadkhan@yahoo.com>
2018-07-01 11:11:27 -0400

    loginusinglocalstorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -12,7 +16,11 @@ import Login from './components/auth/Login';
 
 import './App.css';
 
-
+if(localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,4 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { loginUser } from '../../actions/authActions';
+
 
  class Login extends Component {
   constructor() {
@@ -13,15 +18,25 @@ import React, { Component } from 'react'
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+if(nextProps.auth.isAuthenticated) {
+  this.props.history.push('/dashboard');
+}
+
+if(nextProps.errors){
+  this.setState({errors: nextProps.errors});
+}
+  }
+
     onSubmit(e){
     e.preventDefault();
     
-    const user = {
+    const userData = {
       email: this.state.email,
       password: this.state.password,
     }
     
-    console.log(user);
+    this.props.loginUser(userData);
     
     }
 
@@ -31,6 +46,8 @@ import React, { Component } from 'react'
 
 
   render() {
+const { errors } = this.state;
+
     return (
       <div className="login">
     <div className="container">
@@ -40,18 +57,27 @@ import React, { Component } from 'react'
           <p className="lead text-center">Sign in to your CFDN account</p>
           <form onSubmit={this.onSubmit}>
             <div className="form-group">
-              <input type="email" className="form-control form-control-lg" placeholder="Email Address" 
+              <input type="email" 
+className= {classnames ('form-control form-control-lg', {
+                'is-invalid': errors.email
+              })}  
+              placeholder="Email Address" 
               name="email"
               value={this.state.email}
               onChange={this.onChange}
                />
+  {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
             </div>
             <div className="form-group">
-              <input type="password" className="form-control form-control-lg" placeholder="Password" 
+              <input type="password" 
+className= {classnames ('form-control form-control-lg', { 'is-invalid': errors.password
+              })} 
+              placeholder="Password" 
               name="password"
               value={this.state.password}
               onChange={this.onChange}
                />
+{errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
             </div>
             <input type="submit" className="btn btn-info btn-block mt-4" />
           </form>
@@ -63,5 +89,15 @@ import React, { Component } from 'react'
   }
 }
 
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+}
 
-export default Login;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, { loginUser })(Login);
