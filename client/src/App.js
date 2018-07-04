27742ccb2afd34867c0f31c22e572e8d9999ffb1
unknown <syyadkhan@yahoo.com>
2018-07-04 17:14:24 -0400

    experienceform

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/create-profile/CreateProfile';
 import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
 
 import './App.css';
 
@@ -59,6 +60,9 @@ class App extends Component {
         </Switch>
         <Switch>
           <PrivateRoute exact path="/edit-profile" component={EditProfile} />
+        </Switch>
+        <Switch>
+          <PrivateRoute exact path="/add-experience" component={AddExperience} />
         </Switch>
         </div>
         <Footer />
