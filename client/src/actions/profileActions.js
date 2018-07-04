081f6d51ae444dd9dd5cfaebc8a97f773e2e3715
unknown <syyadkhan@yahoo.com>
2018-07-04 11:26:09 -0400

    profileinputscreated

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from './types';
 
 export const getCurrentProfile = () => dispatch => {
     dispatch(setProfileLoading());
@@ -17,6 +17,18 @@ export const getCurrentProfile = () => dispatch => {
         payload: {}
     })
 );
+};
+
+export const createProfile= (profileData, history) => dispatch => {
+    axios
+    .post('/api/profile', profileData)
+    .then(res => history.push('/dashboard'))
+    .catch(err =>
+    dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+        })
+    );
 }
 
 
--- client/src/components/create-profile/CreateProfile.js
+++ client/src/components/create-profile/CreateProfile.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
 import InputGroup from '../common/InputGroup';
 import SelectListGroup from '../common/SelectListGroup';
+import { createProfile } from '../../actions/profileActions';
 
 
 
@@ -33,9 +35,32 @@ import SelectListGroup from '../common/SelectListGroup';
         this.onSubmit = this.onSubmit.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.errors) {
+            this.setState({errors: nextProps.errors});
+        }
+    }
+
     onSubmit(e) {
         e.preventDefault();
-        console.log('submit');
+        
+        const profileData = {
+                handle: this.state.handle,
+                company: this.state.company,
+                website: this.state.website,
+                location: this.state.location,
+                status: this.state.status,
+                skills: this.state.skills,
+                githubusername: this.state.githubusername,
+                bio: this.state.bio,
+                twitter: this.state.twitter,
+                facebook: this.state.facebook,
+                linkedin: this.state.linkedin,
+                youtube: this.state.youtube,
+                instagram: this.state.instagram
+        };
+
+        this.props.createProfile(profileData, this.props.history);
     }
 
     onChange(e) {
@@ -116,14 +141,14 @@ import SelectListGroup from '../common/SelectListGroup';
       <p className="lead text-center">Please provide some information for your Profile</p>
       <small className="d-block pb-3">* is a required field</small>
       <form onSubmit={this.onSubmit}>
-      <TextFieldGroup 
-      placeholder="* Profile Handle"
-      name="Handle"
-      value={this.state.handle}
-      onChange={this.onChange}
-      error={errors.handle}
-      info="Choose a unique handle for your profile URL, make it cool!"
-      />
+      <TextFieldGroup
+                  placeholder="* Profile Handle"
+                  name="handle"
+                  value={this.state.handle}
+                  onChange={this.onChange}
+                  error={errors.handle}
+                  info="A unique handle for your profile URL. Your full name, company name, nickname"
+                />
       <SelectListGroup 
       placeholder="Status"
       name="status"
@@ -183,7 +208,7 @@ import SelectListGroup from '../common/SelectListGroup';
                   info="Who are you?"
                 />
             <div className="mb-3">
-                <button onClick={() => {
+                <button type="button" onClick={() => {
                     this.setState(prevState => ({
                         displaySocialInputs: !prevState.displaySocialInputs
                         }))
@@ -213,4 +238,4 @@ const mapStateToProps = state => ({
     errors: state.errors
 });
 
-export default connect(mapStateToProps)(CreateProfile);
+export default connect(mapStateToProps, { createProfile })(withRouter(CreateProfile));
