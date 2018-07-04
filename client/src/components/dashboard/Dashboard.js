e6276d9be5ab3cc664f91a9a11488dd7d4dd171c
unknown <syyadkhan@yahoo.com>
2018-07-03 22:30:23 -0400

    dashboardwork

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profileActions';
+import Spinner from '../common/Spinner';
 
 
 class Dashboard extends Component {
@@ -10,14 +12,54 @@ componentDidMount() {
 }
 
   render() {
+      const { user } = this.props.auth;
+      const { profile, loading } = this.props.profile;
+
+      let dashboardContent;
+
+      if(profile === null || loading){
+          dashboardContent = <Spinner />;
+      }else {
+         if(Object.keys(profile).length > 0){
+          dashboardContent = <h4>Display Profile</h4>
+         } else {
+          dashboardContent = (
+            <div>
+              <p className="lead text-muted">Welcome { user.name }</p>
+              <p>Please set up your Profile</p>
+              <Link to="/create-profile" className="btn btn-lg btn-info">
+              Create Profile
+              </Link>
+            </div>
+          );
+         } 
+      }
+
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+              <div className="col-md-12">
+                <h1 className="display-4">Dashboard</h1>
+                {dashboardContent}
+              </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired  
+}
 
-export default connect(null, {getCurrentProfile})(Dashboard);
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
 
