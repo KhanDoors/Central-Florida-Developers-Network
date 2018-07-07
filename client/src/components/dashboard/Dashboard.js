e963abb2918d00682237a4f39844da1265601d65
unknown <syyadkhan@yahoo.com>
2018-07-06 21:48:34 -0400

    educationuidone

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import { getCurrentProfile, deleteAccount } from '../../actions/profileActions';
 import Spinner from '../common/Spinner';
 import ProfileActions from './ProfileActions';
 import Experience from './Experience';
+import Education from './Education';
 
 
 
@@ -33,6 +34,7 @@ onDeleteClick(e) {
               <p className="lead text-muted">Welcome<Link to={`/profile/${profile.handle}`}> { user.name }</Link></p>
               <ProfileActions />            
               <Experience experience={profile.experience} />
+              <Education education={profile.education} />
             <div style={{ marginBottom: '60px'}} />
               <button onClick={this.onDeleteClick.bind(this)} className="btn btn-danger">Delete My Account</button>            
             </div>
--- client/src/components/edit-profile/EditProfile.js
+++ client/src/components/edit-profile/EditProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
@@ -186,6 +186,7 @@ import isEmpty from '../../validation/is-empty';
       <div className="container">
       <div className="row">
       <div className="col-md-8 m-auto">
+      <Link to="/dashboard" className="btn btn-light">Go Back</Link>
       <h1 className="display-4 text-center">Edit Profile</h1>
       <small className="d-block pb-3">* is a required field</small>
       <form onSubmit={this.onSubmit}>
