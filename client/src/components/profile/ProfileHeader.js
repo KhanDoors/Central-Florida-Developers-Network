d1ef48eac0dd1a9e7ebf1f896427b2e8561c3e41
unknown <syyadkhan@yahoo.com>
2018-07-07 22:24:34 -0400

    workingonprofiles

---
+++ client/src/components/profile/ProfileHeader.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+ class ProfileHeader extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Profile Header</h1>
+      </div>
+    )
+  }
+}
+
+
+
+export default ProfileHeader;
