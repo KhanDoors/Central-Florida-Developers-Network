e16a9ee6dafbf10e2a3ffe4d20d5d9fc7a1ea337
unknown <syyadkhan@yahoo.com>
2018-07-07 23:08:02 -0400

    stillworking onprofiles

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -16,13 +16,40 @@ import { getProfileByHandle } from '../../actions/profileActions';
         }
      }
 
-
   render() {
+    const { profile, loading } = this.props.profile;
+    let profileContent;
+
+    if (profile === null || loading) {
+      profileContent = <Spinner />;
+    } else {
+      profileContent = (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <Link to="/profiles" className="btn btn-light mb-3 float-left">
+                Back To Profiles
+              </Link>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <ProfileHeader profile={profile} />
+          <ProfileAbout />
+          <ProfileCreds />
+          <ProfileGithub />
+          </div>
+      );
+    }
+
     return (
-      <div>
-        
+      <div className="profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">{profileContent}</div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
