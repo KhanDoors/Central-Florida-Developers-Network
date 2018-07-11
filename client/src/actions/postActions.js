f44c6a9b098cf4da717027a8196804d9e9719a80
unknown <syyadkhan@yahoo.com>
2018-07-10 21:11:59 -0400

    working on comments

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -110,6 +110,25 @@ export const removeLike = id => dispatch => {
     );
 };
 
+// Add Comment
+export const addComment = (postId, commentData) => dispatch => {
+  
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
   export const setPostLoading = () => {
       return {
           type: POST_LOADING
